619c2287937c5ba4b9be80421c762a37aae564ce
Daniel Lagos <d.f.l.m.0126@gmail.com>
2022-04-28 00:47:16 -0500

    add background image

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -12,11 +12,25 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
       <Head>
         <title>{children}</title>
       </Head>
-      <Navbar/>
-      <main
-        style={{ width: '1440px', padding: '10px 24px', margin: '0 auto' , height: '90vh'}}>
-        {children}
-      </main>
+      <div
+        style={{
+          objectFit: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundImage: 'url(https://images.unsplash.com/photo-1622730000579-e6bde344d6a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80)',
+          backgroundPosition: 'right'
+        }}>
+        <Navbar/>
+        <main
+          style={{
+            width: '1440px',
+            padding: '10px 24px',
+            margin: '0 auto',
+            height: '90vh',
+
+          }}>
+          {children}
+        </main>
+      </div>
     </>
   );
 };
--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,6 @@ import { Layout } from '../components/layouts';
 import { Box, Typography } from '@mui/material';
 
 
-// https://images.unsplash.com/photo-1622730000579-e6bde344d6a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80
 const LoginPage: NextPage = () => {
   return (
     <Layout title={'login'}>
